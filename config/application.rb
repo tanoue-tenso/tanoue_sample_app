d40793786321363d21fac8adea9982f28e65f431
Tanoue Daisuke <tanoue@tenso.com>
2020-07-02 11:29:45 +0900

    titleタグの切り替えテスト,リファクタリング

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module SampleApp
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # webconsoleをdevelopmentのみではなくtestでも実行する
+    config.web_console.development_only = false
   end
 end
