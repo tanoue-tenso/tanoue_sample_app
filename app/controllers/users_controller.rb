a5bd2cbaacd23dbfb64fbe3f90f116d4b9e31ef7
Tanoue Daisuke <tanoue@tenso.com>
2020-07-06 15:54:58 +0900

    gravatarを適用

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    # debugger こいつを差し込むことでレスポンスを送る前にターミナル上で変数の中身とかを見ることができる
+  end
+
   def new
   end
 end
