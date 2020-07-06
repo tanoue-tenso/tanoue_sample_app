b1cd3d0dc2c9b1ca07e7465aa752e09cf3ce757c
Tanoue Daisuke <tanoue@tenso.com>
2020-07-06 17:35:54 +0900

    flashメッセージ実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-
+      # redirect_to @user
+      flash[:success] = "Welcome to the Sample App!!"
+      redirect_to user_url(@user)
     else
       render 'new'
     end
