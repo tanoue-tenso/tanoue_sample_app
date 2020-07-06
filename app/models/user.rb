32d3f31e1069fddc19508cf67a70f011a0580870
Tanoue Daisuke <tanoue@tenso.com>
2020-07-06 12:06:00 +0900

    パスワードのvallidationとそのテスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,7 @@ class User < ActiveRecord::Base
             uniqueness: { case_sensitive: false } # 大文字小文字の区別をしない
 
   has_secure_password
+  validates :password,
+            presence: true, # has_secure_passwordの存在性確認は「更新時」には適用してくれないので追記
+            length: { minimum: 6 }
 end
