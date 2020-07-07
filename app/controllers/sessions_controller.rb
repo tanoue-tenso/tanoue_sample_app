d78319f786e48e267d5dbdfae1b68bdf298e4d50
Tanoue Daisuke <tanoue@tenso.com>
2020-07-07 11:13:24 +0900

    ログインができるようにする

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # ログイン後にリダイレクト
+      log_in user
+      redirect_to user # redirect_to user_url(user)
     else
       flash.now[:danger] = 'Invalid emai/password combination'
       render 'new' # リダイレクトではない => リクエストを送らないのでflashが消えない
