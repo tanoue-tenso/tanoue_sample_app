14f0d640a08310ffcbd455cf0b78120fbd1480ed
Tanoue Daisuke <tanoue@tenso.com>
2020-07-07 15:43:04 +0900

    永続的なセッション実装

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,9 +5,24 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  # ユーザーを永続的セッションに記憶する
+  def remember(user)
+    user.remember # remember_digestを作成(更新)
+    cookies.permanent.signed[:user_id] = user.id # sigendで暗号化, permanentで永続化
+    cookies.permanent[:remember_token] = user.remember_token # ランダムな文字列
+  end
+
   # ログイン中のみログインユーザーを返す
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id]) #
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: session[:user_id]) #
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
   end
 
   # ログインしてる？
@@ -15,8 +30,16 @@ module SessionsHelper
     current_user.present?
   end
 
+  # 永続的セッションを破棄
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
   # ログアウトする
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  attr_accessor :remember_token
   before_save { self.email = self.email.downcase }
   validates :name,
             presence: true,
@@ -16,8 +17,31 @@ class User < ActiveRecord::Base
             presence: true, # has_secure_passwordの存在性確認は「更新時」には適用してくれないので追記
             length: { minimum: 6 }
 
+  # 文字列のハッシュ値を返す
   def self.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+
+  # ランダムなトークンを返す
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  # ハッシュ化させる前に、remember_tokenで仮想的に保存し、ハッシュ化したものをremember_digestに保存
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # 渡されたトークンがダイジェストと一致したらtrueを返す
+  def authenticated?(remember_token)
+    # remember_digestカラムはハッシュ化した remember_token と一致しているか比較している
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # ユーザーログインを破棄
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end
