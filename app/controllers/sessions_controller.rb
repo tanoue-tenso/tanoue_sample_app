1630b73610bc0ca43e9e0cd9f69795d7ce4374b2
Tanoue Daisuke <tanoue@tenso.com>
2020-07-07 10:55:27 +0900

    ログイン時のエラー処理実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # ログイン後にリダイレクト
+    else
+      flash.now[:danger] = 'Invalid emai/password combination'
+      render 'new' # リダイレクトではない => リクエストを送らないのでflashが消えない
+    end
+  end
+
+  def destory
+
+  end
 end
