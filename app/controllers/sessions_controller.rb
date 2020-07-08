e5ae66bf73693915a241a2549afe4a93fea685d6
Tanoue Daisuke <tanoue@tenso.com>
2020-07-08 11:27:10 +0900

    フレンドリーフォワーディングとそのテスト実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       log_in user
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_to user # redirect_to user_url(user)
+      redirect_back_or user # redirect_to user_url(user)
     else
       flash.now[:danger] = 'Invalid emai/password combination'
       render 'new' # リダイレクトではない => リクエストを送らないのでflashが消えない
