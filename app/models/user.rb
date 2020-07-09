53dd0f62ad012d2d9c69230e62eab19d3b7e70f3
Tanoue Daisuke <tanoue@tenso.com>
2020-07-09 12:22:28 +0900

    authenticated?の編集とそのテスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -36,11 +36,11 @@ class User < ActiveRecord::Base
     update_attribute(:remember_digest, User.digest(remember_token))
   end
 
-  # 渡されたトークンがダイジェストと一致したらtrueを返す
-  def authenticated?(remember_token)
-    return false if remember_digest.nil?
-    # remember_digestカラムはハッシュ化した remember_token と一致しているか比較している
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  # 渡されたトークンがダイジェストと一致したらtrueを返す (10.24で更新)
+  def authenticated?(attribute, token)
+    digest = self.send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
   end
 
   # ユーザーログインを破棄
