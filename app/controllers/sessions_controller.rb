d2567a2da6c8e963ac38a937f60e5c0f9201f7d9
Tanoue Daisuke <tanoue@tenso.com>
2020-07-09 12:33:42 +0900

    アカウント有効化実装完了, ログインの制約にactivated適用

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,14 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      log_in user
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_back_or user # redirect_to user_url(user)
+      if user.activated?
+        log_in user
+        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+        redirect_back_or user # redirect_to user_url(user)
+      else
+        message = "Account not activated"
+        messages += "Check your email for the activation link."
+      end
     else
       flash.now[:danger] = 'Invalid emai/password combination'
       render 'new' # リダイレクトではない => リクエストを送らないのでflashが消えない
