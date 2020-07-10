f2a6396b80adb6f5532fdd1b2b0e55d313fc4b5d
Tanoue Daisuke <tanoue@tenso.com>
2020-07-10 15:26:15 +0900

    ユーザーに紐づくmicropost削除

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -65,4 +65,12 @@ class UserTest < ActiveSupport::TestCase
   test "authenticated? should return false for a user with nil digest" do
     assert_not @user.authenticated?(:remember, 'hogehoge')
   end
+
+  test "associated microposts should be destroyed" do
+    @user.save
+    @user.microposts.create!(content: 'テスト〜〜')
+    assert_difference 'Micropost.count', -1 do
+      @user.destroy
+    end
+  end
 end
