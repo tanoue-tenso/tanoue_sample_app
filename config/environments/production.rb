3c2d8f165ac8776b7987e5987f95e56727ebea31
Tanoue Daisuke <tanoue@tenso.com>
2020-07-10 12:42:38 +0900

    本番環境でのメール設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
+  # メール用
+  config.action_mailer.default_url_options = { host: 'tanoue-sample-app.herokuapp.com'}
 end
