b9a902dc65b7da4fe41aa89571afdf05dbea1a18
Tanoue Daisuke <tanoue@tenso.com>
2020-07-10 11:32:10 +0900

    メールのプレビュー表示とトークンの作成,テスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   before_save :down_case_email
   before_create :create_activation_digest
   validates :name,
@@ -57,6 +57,18 @@ class User < ActiveRecord::Base
   # 有効化用のメール送信
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
+  end
+
+  # パスワード再設定の属性を設定する
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest, User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  # パスワード再設定のメール送信
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
   end
 
   private
