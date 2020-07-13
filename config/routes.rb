79a99f71c278f6410f092bbae891c8b48ef9cae1
Tanoue Daisuke <tanoue@tenso.com>
2020-07-13 19:18:49 +0900

    フォロー,アンフォローボタンの切替, これらのroute定義

--- config/routes.rb
+++ config/routes.rb
@@ -22,4 +22,5 @@ Rails.application.routes.draw do
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
   resources :microposts, only: %i(create destroy)
+  resources :relationship, only: %i(create destroy)
 end
