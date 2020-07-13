76e5abafa6bd624d986dda3e2cfbbe3f041de42f
Tanoue Daisuke <tanoue@tenso.com>
2020-07-13 11:20:38 +0900

    micropostの削除機能実装

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,6 @@
 class MicropostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
+  before_action :correct_user, only: :destroy
 
   def create
     @micropost = current_user.microposts.build(micropost_params)
@@ -13,7 +14,9 @@ class MicropostsController < ApplicationController
   end
 
   def destroy
-
+    @micropost.destroy
+    flash[:success] = 'Micropost deleted'
+    redirect_to request.referrer || root_url
   end
 
   private
@@ -21,5 +24,11 @@ class MicropostsController < ApplicationController
     # contentだけweb経由で変更できるようにする
     def micropost_params
       params.require(:micropost).permit(:content)
+    end
+
+    # 正しいユーザーか
+    def correct_user
+      @micropost = current_user.microposts.find_by(id: params[:id])
+      redirect_to root_url if @micropost.nil
     end
 end
